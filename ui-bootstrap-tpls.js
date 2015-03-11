9475a59128e7e8734a50d42d9a996e274cedd27c
GuyPie <perkal@gmail.com>
2015-03-11 14:36:02 +0200

    Update ui-bootstrap-tpls.js

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -3811,17 +3811,19 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
 
       // Keep reference to click handler to unbind it.
       var dismissClickHandler = function (evt) {
-        // Proposed patch: select the clicked item(only if clicking on the typeahead dropdown)
-        if (evt.target.parentElement && evt.target.parentElement.id.indexOf("typeahead") != -1) {
-          scope.$apply(function () {
-            if (scope.activeIdx != -1) {
-              scope.select(scope.activeIdx);
-            }
-          });
+      	$timeout(function() {
+      	  // Proposed patch: select the clicked item(only if clicking on the typeahead dropdown)
+          if (evt.target.parentElement && evt.target.parentElement.id.indexOf("typeahead") != -1) {
+            scope.$apply(function () {
+              if (scope.activeIdx != -1) {
+                scope.select(scope.activeIdx);
+              }
+            });
           
-          resetMatches();
-          scope.$digest();
-        }
+            resetMatches();
+            scope.$digest();
+          }
+      	});
       };
 
       $document.bind('click', dismissClickHandler);
