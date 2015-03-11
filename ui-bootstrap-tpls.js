238fa54fc044e62d4d2f2002929ce49fd0100a77
GuyPie <perkal@gmail.com>
2015-03-11 12:46:40 +0200

    Update ui-bootstrap-tpls.js
    typeahead mouse select fix

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -3811,18 +3811,14 @@ angular.module('ui.bootstrap.typeahead', ['ui.bootstrap.position', 'ui.bootstrap
 
       // Keep reference to click handler to unbind it.
       var dismissClickHandler = function (evt) {
-        if (element[0] !== evt.target) {
-
         // Proposed patch: select the clicked item(only if clicking on the typeahead dropdown)
-        if(evt.target.parentElement.id.indexOf("typeahead") != -1) {
+        if (evt.target.parentElement.id.indexOf("typeahead") != -1) {
           scope.$apply(function () {
             if (scope.activeIdx != -1) {
               scope.select(scope.activeIdx);
             }
-        });
-      }
-      });
-
+          });
+          
           resetMatches();
           scope.$digest();
         }
